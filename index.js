cac127033ce80ec8b2651d30bc126baa7b0bcfe5
JoshElias <josh.elias.dev@gmail.com>
2016-04-06 20:54:23 -0700

    wait till after boot

--- index.js
+++ index.js
@@ -18,53 +18,60 @@ function disconnectDataSource(prop, action, newValue) {
 
 module.exports = function (options) {
   return through.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      this.push(file);
-      cb();
-      return;
-    }
-
-    var app;
-    try {
-      app = require(path.resolve(file.path));
-
-      //  Incase options is undefined.
-      options = options || {ngModuleName: 'lbServices', apiUrl: undefined};
-
-      options.ngModuleName = options.ngModuleName || 'lbServices';
-      options.apiUrl = options.apiUrl || app.get('restApiRoot') || '/api';
-
-      gutil.log('Loaded LoopBack app', gutil.colors.magenta(file.path));
-      gutil.log('Generating',
-        gutil.colors.magenta(options.ngModuleName),
-        'for the API endpoint',
-        gutil.colors.magenta(options.apiUrl)
-      );
-
-      var script = generator.services(
-        app,
-        options.ngModuleName,
-        options.apiUrl
-      );
-
-      file.contents = new Buffer(script);
-
-      gutil.log('Generated Angular services file.');
-
-      this.push(file);
-
-      var dataSources = app.dataSources;
-      for (var dataSource in dataSources) {
-        if (dataSources.hasOwnProperty(dataSource)) {
-          var ds = dataSources[dataSource];
-          watch(ds, 'connected', disconnectDataSource);
-        }
+      if (file.isNull()) {
+          this.push(file);
+          cb();
+          return;
       }
-    } catch (err) {
-      this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
-    }
 
-    cb();
-    return;
+      var app;
+      try {
+          app = require(path.resolve(file.path));
+      } catch (err) {
+          this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
+      }
+
+      app.on('booted', runGenerator);
+
+      function runGenerator() {
+          try {
+              //  Incase options is undefined.
+              options = options || {ngModuleName: 'lbServices', apiUrl: undefined};
+
+              options.ngModuleName = options.ngModuleName || 'lbServices';
+              options.apiUrl = options.apiUrl || app.get('restApiRoot') || '/api';
+
+              gutil.log('Loaded LoopBack app', gutil.colors.magenta(file.path));
+              gutil.log('Generating',
+                  gutil.colors.magenta(options.ngModuleName),
+                  'for the API endpoint',
+                  gutil.colors.magenta(options.apiUrl)
+              );
+
+              var script = generator.services(
+                  app,
+                  options.ngModuleName,
+                  options.apiUrl
+              );
+
+              file.contents = new Buffer(script);
+
+              gutil.log('Generated Angular services file.');
+
+              this.push(file);
+
+              var dataSources = app.dataSources;
+              for (var dataSource in dataSources) {
+                  if (dataSources.hasOwnProperty(dataSource)) {
+                      var ds = dataSources[dataSource];
+                      watch(ds, 'connected', disconnectDataSource);
+                  }
+              }
+          } catch (err) {
+              this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
+          } finally {
+              return cb();
+          }
+      }
   });
 };
